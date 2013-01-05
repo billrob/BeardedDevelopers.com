5dbceafb0f5968356c9d72d4c2c3fb4ed48e2327
jamesjr0204 <jamesjr0204@gmail.com>
2013-01-04 19:37:06 -0600

    Cleaned up code.

---
+++ BD.Web2/BD.Web2/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace BD.Web2.Controllers
+{
+	public class HomeController : Controller
+	{
+		//
+		// GET: /Index/
+
+		public ActionResult Index()
+		{
+			var filename = Server.MapPath("~/app_data/Beards.xml");
+			var doc = new XmlDocument();
+			doc.Load(filename);
+
+			var entry = (BeardedEntry)null;
+
+			var list = new List<BeardedEntry>();
+			foreach (var node in doc.SelectNodes("/beards/entry"))
+			{
+				var xmlNode = (XmlElement)node;
+				entry = new BeardedEntry();
+				entry.Name = (xmlNode.SelectSingleNode("Name").InnerText);
+				entry.Image = (xmlNode.SelectSingleNode("Image").InnerText);
+				entry.Number = Convert.ToInt32(xmlNode.SelectSingleNode("Number").InnerText);
+				entry.FullBeard = Convert.ToBoolean(xmlNode.SelectSingleNode("FullBeard").InnerText);
+				list.Add(entry);
+			}
+			return View(list);
+		}
+
+	}
+}
---
+++ BD.Web2/BD.Web2/Models/BeardedEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BD.Web2  
+{
+    public class BeardedEntry
+    {
+        public int Number { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public bool FullBeard { get; set; }
+    }
+}
